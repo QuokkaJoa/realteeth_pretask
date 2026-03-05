530d4d77f9ed96359e7798295f96174a82442495
quokkajoa <qkadevlog@gmail.com>
2026-03-05 18:13:56 +0900

    refactor: 상세 로깅을 위한 exception class 변경
    - 로깅에 statusCode 추가

--- src/main/kotlin/com/quokkajoa/pretask_realteeth/exception/ExternalException.kt
+++ src/main/kotlin/com/quokkajoa/pretask_realteeth/exception/ExternalException.kt
@@ -2,5 +2,8 @@ package com.quokkajoa.pretask_realteeth.exception
 
 sealed class ExternalException(message: String) : RuntimeException(message)
 
-class ExternalServiceException(val statusCode: Int, message: String) : ExternalException(message)
-class ExternalClientException(val statusCode: Int, message: String) : ExternalException(message)
+class ExternalClientException(val statusCode: Int, message: String)
+    : ExternalException("[$statusCode] $message")
+
+class ExternalServiceException(val statusCode: Int, message: String)
+    : ExternalException("[$statusCode] $message")
